37365ce80593cc514572971ba54de26db3e4573b
aditisingh2310 <singh.riya20062010@gmail.com>
2026-01-15 08:33:29 +0000

    Restructure repository: organize code, config, scripts, tests, and docs into conventional folders

--- logger_enterprise.js
+++
@@ -1,5 +0,0 @@
-import { logger } from '../utils/winston-logger-enterprise.js';
-export const requestLogger = (req, res, next) => {
-  logger.info({ method: req.method, url: req.url });
-  next();
-};
--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  use: {
-    headless: true,
-    viewport: { width: 1280, height: 800 },
-    actionTimeout: 10000,
-  },
-  retries: 1,
-  testDir: './tests'
-}
