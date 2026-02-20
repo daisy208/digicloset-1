ffb0604accc17844a59567790a58ea3d55d666dd
aditisingh2310 <singh.riya20062010@gmail.com>
2026-02-20 12:04:26 +0530

    Update logging_conf_enterprise.py

--- python/app/logging_conf_enterprise.py
+++ python/app/logging_conf_enterprise.py
@@ -5,3 +5,10 @@ def setup_logging():
         format="%(asctime)s %(levelname)s %(name)s - %(message)s",
         handlers=[logging.FileHandler("app.log"), logging.StreamHandler()],
     )
+    import os
+
+SHOPIFY_API_KEY = os.getenv("SHOPIFY_API_KEY")
+SHOPIFY_API_SECRET = os.getenv("SHOPIFY_API_SECRET")
+SHOPIFY_SCOPES = "read_products,write_products"
+SHOPIFY_API_VERSION = "2024-01"
+SHOPIFY_REDIRECT_URI = os.getenv("SHOPIFY_REDIRECT_URI")
